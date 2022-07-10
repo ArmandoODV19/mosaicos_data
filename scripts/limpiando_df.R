52c2fc0ba4989f3a1cf74da43b842f2a7d542893
armandios19 <fooseaov@gmail.com>
2022-07-10 17:30:51 -0500

    haciendo el grafico

--- scripts/limpiando_df.R
+++ scripts/limpiando_df.R
@@ -6,3 +6,15 @@ mosaicos <- read_csv("data/mosaicos_df.csv")
 mosaicos$frecuencia <- gsub(pattern = " ", replacement = "", x = mosaicos$frecuencia)
 
 mosaicos$frecuencia <- as.numeric(mosaicos$frecuencia)
+
+# haciendo los graficos
+
+mosaicos %>%
+  filter(clase == "I") %>%
+  ggplot(aes(x = enfermedad, y = frecuencia, color = edad, group = edad)) +
+  geom_line() +
+  geom_point(size = 6)+
+  xlab("")
+
+
+
